05db815f25846d61cab0a6a562910c3e22ef1f89
Freddy <freddy.a.monterroso@gmail.com>
2023-06-11 16:00:20 -0600

    Practica 2

--- APISOAP/index.js
+++ APISOAP/index.js
@@ -4,9 +4,56 @@ const { parseStringPromise } = require('xml2js');
 const cors = require('cors');
 
 const app = express();
+app.use(express.json())
 const PORT = 3000;
 
 app.use(cors()); // Aplicar la configuración de CORS
+
+const clientId = 'sa';
+const clientSecret = 'fb5089840031449f1a4bf2c91c2bd2261d5b2f122bd8754ffe23be17b107b8eb103b441de3771745';
+
+// Almacenar el token de acceso
+let accessToken = '';
+
+// Endpoint para generar el token de acceso
+app.post('/api/token', async (req, res) => {
+  try {
+    // Verificar las credenciales del cliente
+    const { client_id, client_secret } = req.body;
+    if (client_id !== clientId || client_secret !== clientSecret) {
+      return res.status(401).json({ error: 'Credenciales inválidas' });
+    }
+
+    // Generar un nuevo token de acceso (puede implementar su lógica personalizada aquí)
+    accessToken = '201212854SA';
+
+    // Retornar el token de acceso
+    res.json({ access_token: accessToken });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al generar el token de acceso' });
+  }
+});
+
+// Middleware para verificar el token de acceso en las rutas protegidas
+const verifyToken = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  // Verificar si se proporcionó el token de acceso en el encabezado Authorization
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return res.status(401).json({ error: 'Acceso no autorizado' });
+  }
+
+  const token = authorization.split(' ')[1];
+
+  // Verificar si el token de acceso es válido
+  if (token !== accessToken) {
+    return res.status(401).json({ error: 'Acceso no autorizado' });
+  }
+
+  next();
+};
+
 
 app.get('/api/country/', async (req, res) => {
     try {
--- practica1_201212854/src/app/folder/folder.page.ts
+++ practica1_201212854/src/app/folder/folder.page.ts
@@ -50,14 +50,22 @@ export class FolderPage implements OnInit {
     Se creo una API especial para consumir la API SOAP por problemas de CORS 
   */
   getAllCountry() {
+    const clientId = 'sa';
+    const clientSecret = 'fb5089840031449f1a4bf2c91c2bd2261d5b2f122bd8754ffe23be17b107b8eb103b441de3771745';
+
     this.config.currentPage = 1;
-    this.httpclient.get<any>("http://localhost:3000/api/country").subscribe((data => {      
-      for (let index = 0; index < data.countries.length; index++) {
-        const name = data.countries[index]["m:sName"];
-        const capital = data.countries[index]["m:sCapitalCity"];
-        this.listCountrys.push({name, capital});
-      }
-    }));
+    this.httpclient.post<any>("http://localhost:3000/api/token", { "client_id": clientId, "client_secret": clientSecret}).subscribe(data => {
+      console.log(data);
+      this.httpclient.get<any>("http://localhost:3000/api/country").subscribe((data => {      
+        for (let index = 0; index < data.countries.length; index++) {
+          const name = data.countries[index]["m:sName"];
+          const capital = data.countries[index]["m:sCapitalCity"];
+          this.listCountrys.push({name, capital});
+        }
+      }));
+    });
+
+    
   }
 
   pageChanged(event: any) {
